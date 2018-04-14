5fa5c7899892bf25f75f719572eac9a16ad54a04
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-14 11:25:36 +0200

    Documentation

--- teensy.ino
+++ teensy.ino
@@ -1,7 +1,14 @@
-int redLed = 21;
-int greenLed = 22;
+// Connection
 
-int sensor = 23;
+// GMD: 1. od X
+// VCC: 8. od X
+
+int redLed = 21; // 4. od X
+int greenLed = 22; // 6. od X
+
+int sensor = 23; // 7. od X
+
+// Setup
 
 void setup() {
 	pinMode(redLed, OUTPUT);
@@ -12,27 +19,38 @@ void setup() {
 	Serial.begin(115200);
 }
 
+// For storing analog value
 float read;
 
+// Red - read -red - green - read - green loop
 void loop() {
+	// Sync
+
 	Serial.println("");
+
+	// red : off ; green : on
 
 	digitalWrite(redLed, HIGH);
 	digitalWrite(greenLed, LOW);
 	delay(100);
 
+	// measure green
 	read = analogRead(sensor);
 
+	// send green over serial
 	Serial.println(read);
 	
 	delay(100);
 	
+	// red: on ; green: off
 	digitalWrite(redLed, LOW);
 	digitalWrite(greenLed, HIGH);
 	delay(100);
 
+	// measure red
 	read = analogRead(sensor);
 
+	// send red
 	Serial.println(read);
 	
 	delay(100);
